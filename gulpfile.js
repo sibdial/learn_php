951ad9f7676ac9d64329c620c7da42f699125eae
sibdial <d.a.sibiryakov@bk.ru>
2016-05-09 21:44:52 +0600

    начинаем учить php

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+	var gulp = require('gulp'),
+		connect = require('gulp-connect'),
+		opn = require('opn');
+
+	gulp.task('connect', function() {
+	  connect.server({
+	    root: 'app',
+	    livereload: true,
+	    port: '9999'
+	  });
+	  opn('http://learning.loc')
+	});
+
+	gulp.task('php', function () {
+	  gulp.src('./app/*.php')
+	    .pipe(connect.reload());
+	});
+
+	gulp.task('css', function () {
+	  gulp.src('./app/css/*.css')
+	    .pipe(connect.reload());
+	});
+
+	gulp.task('js', function () {
+	  gulp.src('./app/js/*.js')
+	    .pipe(connect.reload());
+	});
+
+	gulp.task('watch', function () {
+	   gulp.watch(['./app/*.php'], ['php']);
+	   gulp.watch(['./app/css/*.css'], ['css']);
+	   gulp.watch(['./app/js/*.js'], ['js']);
+	});
+
+	gulp.task('default', ['connect', 'watch']);
